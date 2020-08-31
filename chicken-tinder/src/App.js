865f7427df3b38630fdc75a49c2e67da65c11925
Melynda Schreiber <mschreib@udel.edu>
2020-08-30 20:02:16 -0600

    Updated the files to include a swiping feature

--- chicken-tinder/src/App.js
+++ chicken-tinder/src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+// import ButtonAppBar from './AppHeader';
+import config from './config';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import TinderCards from './TinderCards';
+// import TinderCard from react-tinder-card
+
+var mykey = config.MY_KEY;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<ButtonAppBar />*/}
+      {/* Header */}
+      <Header/>
+      
+      <Router>
+        
+        <Switch>
+          <Route path='/chat'>
+            <h1> This is my chat.</h1>
+          </Route>
+          <Route>
+            <TinderCards />
+          </Route>
+
+        </Switch>
+
+      </Router>
+
+      <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{mykey}}&libraries=places"></script>
+      {/* Tinder Cards */} 
+      {/* Buttons Below the cards*/}
+      
+      {/* Chat Screen */}
+      {/* Individual Chat Screen */}
+
     </div>
   );
 }
